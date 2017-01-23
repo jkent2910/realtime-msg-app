76019d79c69b79c8494a9a52aaa43dd1e38c6548
Julie Kent <jkent2910@gmail.com>
2017-01-23 14:55:55 -0700

    user model updates

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context "relationships" do
+    it "has many messages" do
+      expect(subject).to have_many(:messages)
+    end
+  end
+
 end
