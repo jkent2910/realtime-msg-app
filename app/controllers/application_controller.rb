2ac3e1f963264bc23199f6d8613be4a74e15a4c6
Julie Kent <jkent2910@gmail.com>
2017-01-23 15:26:27 -0700

    redirect after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def after_sign_in_path_for(resource)
+    messages_path
+  end
 
   rescue_from ActiveRecord::RecordNotFound do
     flash[:warning] = 'Resource not found.'
